2e1a5b162aef8ad6a573963bc5a79eeb93abc191
osamah waleed <ostud08@gmail.com>
2026-04-29 16:50:38 +0300

    first version of AI detection website

---
+++ README.dataset.txt
@@ -0,0 +1,6 @@
+# PPE-Detection-with-Gloves > 2023-08-21 12:33am
+https://universe.roboflow.com/ppe-detection-82plm/ppe-detection-with-gloves
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request
+from ultralytics import YOLO
+import os
+
+app = Flask(__name__)
+
+# تحميل المودل
+model = YOLO("runs/detect/train/weights/best.pt")
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route("/")
+def home():
+    return '''
+    <h2>Upload Image for Detection</h2>
+    <form action="/predict" method="post" enctype="multipart/form-data">
+        <input type="file" name="image">
+        <button type="submit">Upload</button>
+    </form>
+    '''
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    file = request.files["image"]
+
+    filepath = os.path.join(UPLOAD_FOLDER, file.filename)
+    file.save(filepath)
+
+    results = model(filepath)
+
+    results[0].save()
+
+    return "Detection completed! Check runs/detect/predict"
+
+if __name__ == "__main__":
+    app.run(port=5000)
