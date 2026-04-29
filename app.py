03d6d7c7b516996a4f0e5ebe3eb84d1e1eb1597a
osamah-builder <ostud08@gmail.com>
2026-04-29 18:55:36 +0300

    fix host and port for render

--- app.py
+++ app.py
@@ -34,5 +34,8 @@ def predict():
 
     return "Detection completed! Check runs/detect/predict"
 
+import os
+
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
